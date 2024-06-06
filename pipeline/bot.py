02ec622eacf91b9b140c125ed9052b9b58000e30
nixiieee <xenz5240@gmail.com>
2024-06-07 00:20:29 +0300

    change bot

--- pipeline/bot.py
+++ pipeline/bot.py
@@ -20,18 +20,17 @@ def start(message):
 @bot.message_handler(content_types=['document', 'audio'])
 def handle_file(message):
     sent_message = bot.reply_to(message, "⏳ Processing your file...")
-    file_extension = os.path.splitext(message.document.file_name)[-1][1:].lower()
 
-    if file_extension in ['ogg', 'wav', 'mp3']:
-        file_id = message.document.file_id
-        if message.content_type == 'document':
-            file_info = bot.get_file(message.document.file_id)
-            downloaded_file = bot.download_file(file_info.file_path)
-        if message.content_type == 'audio':
-            file_info = bot.get_file(message.audio.file_id)
-            downloaded_file = bot.download_file(file_info.file_path)
-
+    if message.content_type == 'document':
+        file_extension = os.path.splitext(message.document.file_name)[-1][1:].lower()
+        file_info = bot.get_file(message.document.file_id)
         src = 'files/' + message.document.file_name
+    if message.content_type == 'audio':
+        file_extension = os.path.splitext(message.audio.file_name)[-1][1:].lower()
+        file_info = bot.get_file(message.audio.file_id)
+        src = 'files/' + message.audio.file_name
+    if file_extension in ['ogg', 'wav', 'mp3']:
+        downloaded_file = bot.download_file(file_info.file_path)
         with open(src, 'wb') as new_file:
             new_file.write(downloaded_file)
 
@@ -46,6 +45,7 @@ def handle_file(message):
         bot.edit_message_text(chat_id=message.chat.id, message_id=sent_message.message_id, text=f"🎉 Prediction: {prediction}")
     else:
         bot.edit_message_text(chat_id=message.chat.id, message_id=sent_message.message_id, text="🚫 Only audio files (WAV, or MP3) are supported.")
+
 
 if __name__ == '__main__':
     bot.polling()
